fb201f29f6c144221a74ce26cff8b1bde1eaed42
azartechnet <azartechnet@gmail.com>
2024-02-17 07:50:18 +0530

    updated

---
+++ WebFormUI/EventHandling.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormUI
+{
+    public partial class EventHandling : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(firstvalue.Text);
+            int b = Convert.ToInt32(secondvalue.Text);
+            total.Text = (a + b).ToString();
+        }
+    }
+}
