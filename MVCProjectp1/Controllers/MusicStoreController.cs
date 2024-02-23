ebd75b8170a508eee4bdeb1cba63d10cdc00858a
azartechnet <azartechnet@gmail.com>
2024-02-23 13:21:07 +0530

    updated

---
+++ MVCProjectp1/Controllers/MusicStoreController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProjectp1.Controllers
+{
+    public class MusicStoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome()
+        {
+            return "Hello this is message for action";
+        }
+        [ActionName("ShowMusicApp")]
+        public string ShowMusic(string MusicTitle)
+        {
+            return "You Selected" + MusicTitle + "Music";
+        }
+      
+        [HttpPost]
+        public ActionResult PostMusic()
+        {
+            return  View();
+        }
+    }
+}
